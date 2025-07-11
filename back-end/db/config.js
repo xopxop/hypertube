8ca455c1d87956529f48a9cf968adc248d8ec944
du than <du.than@relexsolutions.com>
2025-07-11 15:54:47 +0300

    refactor: replace dotenv configuration with direct imports for PostgreSQL connection settings

---
+++ back-end/db/config.js
@@ -0,0 +1,7 @@
+const USERNAME = "admin"
+const PASSWORD = "admin"
+const HOST = "localhost"
+const PORT = 5432
+const DB = "hypertube"
+
+export { USERNAME, PASSWORD, HOST, PORT, DB };
--- back-end/db/db.js
+++ back-end/db/db.js
@@ -1,15 +1,12 @@
 import { Pool } from "pg";
-import dotenv from "dotenv";
-dotenv.config({ path: "../.env.common" });
-
-const { POSTGRES_USERNAME, POSTGRES_PASSWORD, POSTGRES_HOST, POSTGRES_PORT, POSTGRES_DB } = process.env;
+import { USERNAME, PASSWORD, HOST, PORT, DB } from "./config.js";
 
 const pool = new Pool({
-  host: POSTGRES_HOST,
-  port: POSTGRES_PORT,
-  database: POSTGRES_DB,
-  user: POSTGRES_USERNAME,
-  password: POSTGRES_PASSWORD
+  host: HOST,
+  port: PORT,
+  database: DB,
+  user: USERNAME,
+  password: PASSWORD
 });
 
 const connectToPostgreSqlAsync = async () => {
